e6586f6309204d795cbd24f5b30e8291de173644
Alfonso <fonsocarre@gmail.com>
2014-02-09 20:11:45 +0100

    Initial Commit

---
+++ GAero/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GAero
+//
+//  Created by Alfonso Carre on 09/02/14.
+//  Copyright (c) 2014 Alfonso Carre. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
