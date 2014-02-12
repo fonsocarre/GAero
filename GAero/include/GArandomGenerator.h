26fcf6a6f901db5083e6fe39aec4344aea19e252
Alfonso <fonsocarre@gmail.com>
2014-02-12 17:38:15 +0100

    Mas

---
+++ GAero/include/GArandomGenerator.h
@@ -0,0 +1,32 @@
+//
+//  randomGenerator.h
+//  GAero
+//
+//  Created by Alfonso Carre on 11/02/14.
+//  Copyright (c) 2014 Alfonso Carre. All rights reserved.
+//
+
+#ifndef __GAero__randomGenerator__
+#define __GAero__randomGenerator__
+
+#include <time.h>
+#include <vector>
+#include "randomc.h"
+
+class GArandom {
+    int seed;
+    CRandomMersenne generator;
+
+public:
+    GArandom();
+    bool flipCoin();
+    bool flipPounderedCoin(double trueProb);
+    int chooseInt(int low, int high);
+    int roulette(std::vector<double> weights);
+    double randDouble();
+};
+
+
+
+
+#endif /* defined(__GAero__randomGenerator__) */
