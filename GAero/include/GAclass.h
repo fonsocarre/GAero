59f292dc4acfa5aaa6bdef6b8433566594cb4e8a
Alfonso <fonsocarre@gmail.com>
2014-04-01 17:27:13 +0200

    Doxy Documentation

--- GAero/include/GAclass.h
+++ GAero/include/GAclass.h
@@ -21,10 +21,14 @@
 #include "utilities.h"
 #include "GAairfoilFitness.h"
 
-
+//! The main Genetic Algorithm class.
+/*! This class implements all the necessary variables and functions
+    needed for this GA. */
 class GAclass {
     // class attributes
+    //! Number of individuals per generation.
     int nPopulation;
+    //! Temporary vector of previous generation population.
     std::vector<GApopulation> oldPopulation;
     std::vector<GApopulation> population;
     GAsettingsClass GAsettings;
@@ -34,11 +38,21 @@ class GAclass {
     int usedPopulation;
     
     // private class members
+    //! Outputs to file and screen the info about the
+    //! population. Only works in fitness-ordered sets of pop.
     void getPopFitness();
+    //! Calculates the fitness of all the NEW population.
     void calculatePopFitness();
+    //! Calculates the fitness of the oldPopulation.
     void calculateOldPopFitness();
+    //! Step of evolve() method. Copies the elitists individuals to the
+    //! next generation.
     void evolveElitists();
+    //! Step of evolve(), crosses the individuals based on roulette
+    //! selection.
     void crossIndividuals();
+    //! Last step of evolve(). Fills the empty population
+    //! with new random inidividuals.
     void createNewIndividuals();
     std::vector<double> oldPopFitness2vec();
     
@@ -46,23 +60,45 @@ class GAclass {
 
 public:
     // Constructors
+    //! Basic constructor.
+    //! Initialises all the fields to 0 or empty vectors.
+    //! Random generator is initialised with date-based seed.
     GAclass();
+    //! Constructor for GAclass. Input nPop is used
+    //! to initialise the population vector reserving memory.
     GAclass(int nPop);
+    //! Principal constructor.
+    //! Using the config4cpp for reading the settings file
+    //! and initialise the vector allocating the necessary
+    //! resources.
     GAclass(const char* settingsFile);
+    //! Another basic constructor.
     GAclass(int nPop, int genomeSize);
+    //! Destructor. Deallocates the vector variables.
     ~GAclass();
     
     // public attributes
+    
+    //! Random number generator. Uses the randomc library
+    //! for improving the uniformity of random number.
     GArandom randomGen;
+    //! Ostream for output to file (specified in the settings file)
+    //! and screen.
     GAoutputclass GAout;
+    //! Number of max generations in the algorithm.
     int nGenerations;
+    //! Counter for generations.
     int iGeneration;
     
     // Member functions
+    
+    //! GA basic routine. Applies the GA steps for each generation
+    //! when finished, a new generation is created and the old one
+    //! is copied to GAclass::oldPopulation.
     void evolve();
     void initPop();
     
-    double FitnessFunction(std::vector<double> genome);
+    //double FitnessFunction(std::vector<double> genome);
     GAfitness fitness;
     
 };
