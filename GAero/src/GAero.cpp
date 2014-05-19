0c7661dba7a73e0f471c58df41a363a8224b0c56
Alfonso del Carre <fonsocarre@gmail.com>
2014-05-19 14:23:08 +0200

    Spline interpolation

--- GAero/src/GAero.cpp
+++ GAero/src/GAero.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  GAlgorithm
+//  GAero
 //
 //  Created by Alfonso Carre on 04/02/14.
 //  Copyright (c) 2014 Alfonso Carre. All rights reserved.
@@ -8,15 +8,13 @@
 
 #include "GAclass.h"
 
-
 int main(int argc, const char * argv[])
 {
-
     GAclass GA("/Users/fonso/C++/GAero/GAero/Settings/settings.cfg");
     
     GA.initPop();
     
-    std::cout << GA.nGenerations << std::endl;
+    //std::cout << GA.nGenerations << std::endl;
     for (GA.iGeneration=0; GA.iGeneration<GA.nGenerations; GA.iGeneration++)
     {
         GA.evolve();
