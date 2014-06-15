47894217ded234f6bf6eb962667fe3ed8023ed72
Alfonso Carre <fonsocarre@gmail.com>
2014-06-15 15:45:50 +0200

    More time measurement, now real time too.

--- GAero/src/GAero.cpp
+++ GAero/src/GAero.cpp
@@ -14,12 +14,13 @@
 int main(int argc, const char * argv[])
 {
     std::clock_t begin = clock();
+    std::chrono::time_point<std::chrono::system_clock> cstart, cend;
+    cstart = std::chrono::system_clock::now();
     
     GAclass GA("/Users/fonso/C++/GAero/GAero/Settings/settings.cfg");
     
     GA.initPop();
     
-    //std::cout << GA.nGenerations << std::endl;
     for (GA.iGeneration=0; GA.iGeneration<GA.nGenerations; GA.iGeneration++)
     {
         GA.evolve();
@@ -27,8 +28,14 @@ int main(int argc, const char * argv[])
     }
     
     std::clock_t end = clock();
-    std::cout << "Time elapsed: " << float(end-begin)/CLOCKS_PER_SEC;
+    cend = std::chrono::system_clock::now();
+    std::cout << "CPU time elapsed: " << float(end-begin)/CLOCKS_PER_SEC;
     std::cout << std::endl;
+    
+    std::chrono::duration<double> elapsed_seconds = cend-cstart;
+    std::cout << "Real time elapsed: " << elapsed_seconds.count()
+              << "\n";
+    
     
     return 0;
 }
